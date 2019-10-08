01cc0921776a1a182749e481e80665a871402d4f
Hugo Rojas <hurojas.rios@gmail.com>
2019-10-08 13:11:36 -0300

    sasasa

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,20 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "../../styles/home.scss";
 
 export class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light mb-3">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
+			<nav className=" navbar navbar-light bg-light">
+				<a className="navbar-brand" href="#">
+					<img src="imagenes/libro.jpg " width="100px" />
+				</a>
+
+				<div className="col md">
+					<span>INICIO</span>
+				</div>
+				<div className="col md">
+					<span>CONTACTO</span>
 				</div>
 			</nav>
 		);
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -5,15 +5,11 @@ import "../../styles/home.scss";
 export class Home extends React.Component {
 	render() {
 		return (
-			<div className="text-center mt-5">
-				<h1>Hello Rigo!</h1>
-				<p>
-					<img src={rigoImage} />
-				</p>
-				<a href="#" className="btn btn-success">
-					If you see this green button, bootstrap is working
-				</a>
-			</div>
+			<header className="home" style={{ backgroundImage: "url(imagenes/foto.png)" }}>
+				<div id="logo1">
+					<h1> KAJINMUEBLERIA</h1>
+				</div>
+			</header>
 		);
 	}
 }
